2711bfc5407c9e8d9fa5517e9e8425bd65b57a20
Jesús Lizárraga <jesuslizarraga@Jesuss-MacBook-Pro.local>
2022-01-04 18:28:50 -0600

    Change how react is imported to * notation

--- wp-content/themes/d-wedding/src/js/app.tsx
+++ wp-content/themes/d-wedding/src/js/app.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 import { useEffect, useState } from 'react'
 import { ApiRoutes } from './utils/api-routes'
 import Banner from './sections/Banner'
